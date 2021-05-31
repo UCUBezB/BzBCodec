0dc42a31535ad1ae01742a048e4901cab8595f4a
Bohdan Shevchuk <shevdan007@gmail.com>
2021-06-01 00:54:16 +0300

    Update convert

--- src/convert.py
+++ src/convert.py
@@ -7,7 +7,10 @@ from PIL import Image
 from moviepy.editor import VideoFileClip
 from pydub import AudioSegment
 import os
-
+from lzw import lzw_compress, lzw_decompress
+from Huffman_algo import HuffmanCode
+from lz77 import compress
+from deflate import Deflate
 
 
 class Convert:
@@ -34,19 +37,36 @@ class Convert:
     save()
         compresses any given file or raises the error if it is unsupported
     '''
-    def __init__(self, path: str):
+    def __init__(self, path: str, compression_type='lz77'):
         if not os.path.exists(path):
             raise TypeError('You must provide a valid path')
 
         self.path = path
+        self.compress = self.compresssion(compression_type)
+
+    def compresssion(self, compression_type):
+        if compression_type.lower() == 'lz77':
+            return compress
+        if compression_type.lower() == 'lzw':
+            return lzw_compress
+        if compression_type.lower() == 'deflate':
+            return Deflate().encode
+        if 'deflate' in compression_type.lower():
+            return HuffmanCode().encode
+        raise ValueError('Unsupported compression. One of the following are supported:\
+ lz77, lzw, huffman, deflate')
 
     def _convert_img(self, image) -> tuple:
         '''
         Private method for converting image into numpy array
         '''
         arr = np.array(image)
-        shape = np.array(arr.shape)
-        flat_arr = arr.ravel()
+        shape = np.array(arr.shape, dtype='uint8')
+        flat_arr = self.compress(arr.ravel())
+
+        # flat_arr = np.array(flat_arr, dtype=[('offset', 'uint8'), ('length', 'uint16'), ('pixel', 'uint8')])
+        # print(flat_arr)
+
         return flat_arr, shape
 
     def save_img(self):
@@ -56,9 +76,10 @@ class Convert:
         img = Image.open(self.path).convert('RGB')
         arr, shape = self._convert_img(img)
         img_info = np.array([arr, shape])
-        # insert compression here
-        with open(f'{self.path[:-3]}bzbi', 'wb') as file:
-            np.save(file, img_info)
+
+
+        # with open(f'{self.path[:-3]}bzbi', 'wb') as file:
+        np.savez_compressed(f'{self.path[:-3]}bzbi', img_info)
 
     def save_vid(self):
         '''
@@ -66,24 +87,25 @@ class Convert:
         '''
         clip = VideoFileClip(self.path)
         rate = clip.fps
+        print(rate)
         size = clip.size[::-1]
         vid_info = np.array([rate, *size, 3.], dtype = np.int)
-        print(vid_info)
-        clip = clip.subclip(0, 2)
 
         #convert frames of the video into numpy
         frames = []
         for frame in clip.iter_frames():
+            frame = np.array(frame, dtype='uint8')
             #insert compression here
-            frame_arr = frame.ravel()
+            frame_arr = self.compress(frame.ravel())
+
             frames.append(frame_arr)
 
         frames = np.array(frames)
 
         frames = np.array([frames, vid_info])
 
-        with open(f'{self.path[:-3]}bzbv', 'wb') as file:
-            np.save(file, frames)
+        # with open(f'{self.path[:-3]}bzbv', 'wb') as file:
+        np.savez_compressed(f'{self.path[:-3]}bzbv', frames)
 
     def save_audio(self):
         '''
@@ -101,27 +123,34 @@ class Convert:
 
         package = []
         for cnt in range(round(sound.frame_count()/ sound.frame_rate)):
-            
-            #insert compression here
 
-            package.append(raw[(cnt * sound.frame_rate) : ((cnt + 1)*sound.frame_rate)])
+            compressed_package = np.array(raw[(cnt * sound.frame_rate) : ((cnt + 1)*sound.frame_rate)], dtype='uint16')
+            #insert compression here
+            compressed_package = self.compress(compressed_package)
+
+            package.append(compressed_package)
         
         package = np.array(package)
         audio = np.array([package, audio_info])
 
-        with open(f'{self.path[:-3]}bzba', 'wb') as file:
-            np.save(file, audio)
+        np.savez_compressed(f'{self.path[:-3]}bzba', audio)
 
     
     def save(self):
         '''
         compresses any given file or raises the error if it is unsupported
         '''
-        if self.path.endswith('.jpg') or self.path.endswith('.png'):
+        if self.path.endswith('.jpg') or self.path.endswith('.png') or self.path.endswith('.jpeg'):
             self.save_img()
-        elif self.path.endswith('mp4'):
+        elif self.path.endswith('mp4') or self.path.endswith('mov'):
             self.save_vid()
         elif self.path.endswith('mp3'):
             self.save_audio()
         else:
             raise ValueError('Currently unsupported file.')
+
+if __name__ == '__main__':
+    path = './examples/test_audio.mp3'
+    
+    conv = Convert(path)
+    conv.save()
